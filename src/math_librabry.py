e3be6947ff4fd136cf7cb654fbba58950f3cdad4
vitekk02 <79334309+vitekk02@users.noreply.github.com>
2021-03-06 15:12:06 +0100

    Added math library header and tests
    Run tests with : python -m unittest -v tests_math_library.py

---
+++ src/math_librabry.py
@@ -0,0 +1,39 @@
+
+
+def addition():
+    return
+
+def subtraction():
+    return
+
+def multiplication():
+    return
+
+def division():
+    return
+
+def factorial():
+    return
+
+def sqrt():
+    return
+
+def exponent():
+    return
+
+def abs():
+    return
+
+def logarithm():
+    return
+
+
+def sine():
+    return
+
+def cosine():
+    return
+
+def tangent():
+    return
+
