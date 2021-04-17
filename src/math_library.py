8d353352d8fe23e3ae32bf29106d198e6f653d23
Matej <matej.slivka33@gmail.com>
2021-04-17 20:34:02 +0200

    Addition of root and square root function

--- src/math_library.py
+++ src/math_library.py
@@ -27,8 +27,33 @@ def factorial(a):
     return product
 
 
-def sqrt():
-    return
+def sqrt(a, b):
+    return root(a, b)
+
+
+def root(a,b):
+    if a < 1:
+        raise TypeError("Calling root lower than 1")
+    if not type(a) is int:
+        raise TypeError("Calling non-integer root")
+    if a%2==0 and b < 0:
+        raise Exception("Calling even root of negative number.")
+    if b==0 or b==1 or b==-1:
+        return b
+    aprox=(absolute(b)+1)/2
+    delta=(absolute(b)+1)/4
+    for i in range(0,64):
+        if exponent(aprox,a)==absolute(b):
+            break
+        if exponent(aprox,a)>absolute(b):
+            aprox=aprox-delta
+        else:
+            aprox=aprox+delta
+        delta=delta/2
+    if b < 0:
+        return -aprox
+    if b > 0:
+        return aprox
 
 
 def exponent(a, b):
