4de00e614168462cd671f7212b9fbad682f8c0a5
Matej <matej.slivka33@gmail.com>
2021-04-19 20:18:10 +0200

    Fixed profiling so that it can work with negative numbers and floats. Modified script so that it works with arguments

--- src/profiling.py
+++ src/profiling.py
@@ -1,25 +1,26 @@
 import math_library as mlib
+import sys
 num = ""
 _NUMBERS = []
 count= 0
-with open("data.txt", 'r') as file:
+with open(sys.argv[1], 'r') as file:
     oread = file.read(1)
+    sign = 1
     while oread != '':
-        if ord(oread) >= 48 and ord(oread) <= 57 :
+        if ord(oread) != 10 and ord(oread) != 9 and ord(oread) != 32 :
             num = num + oread
         else:
-            _NUMBERS += [num]
+            _NUMBERS += [num*sign]
             count += 1
             num = ''
         oread = file.read(1)
 avg = 0
 for i in _NUMBERS:
-    avg = mlib.add(avg, int(i))
+    avg = mlib.add(avg, float(i))
 avg = mlib.divide(avg, count)
-print(avg)
 s = 0
 for i in _NUMBERS:
-    s = mlib.add(s ,mlib.exponentiate(int(i), 2))
+    s = mlib.add(s ,mlib.exponentiate(float(i), 2))
 s = mlib.subtract(s, mlib.multiply(count, mlib.exponentiate(avg,2)))
 s = mlib.divide(s, mlib.subtract(count, 1))
 s = mlib.root(s, 2)
