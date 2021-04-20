868d34912efbc90cecf3ca276c5c3378cb53a747
Matej <matej.slivka33@gmail.com>
2021-04-20 20:18:23 +0200

    Changed profiling so that it works for each line individualy

--- src/profiling.py
+++ src/profiling.py
@@ -1,39 +1,31 @@
 import math_library as mlib
 import sys
 
-num = ""
 _NUMBERS = []
-count = 0
 
-if (len(sys.argv) != 2):
-    print(f'Usage: {sys.argv[0]} input_file')
-    sys.exit(1)
-
-with open(sys.argv[1], 'r') as file:
-    oread = file.read(1)
-    sign = 1
+#if (len(sys.argv) != 2):
+  #  print(f'Usage: {sys.argv[0]} input_file')
+ #   sys.exit(1)
+#with open(sys.argv[1], 'r') as file:
+with open('data.txt', 'r') as file:
+    oread = file.readline()
     while oread != '':
-        if ord(oread) != 10 and ord(oread) != 9 and ord(oread) != 32: # magic numbers, maybe get rid of this?
-            num = num + oread
+        _NUMBERS = oread.split()
 
-        else:
-            _NUMBERS += [num*sign]
+        avg = 0
+        count = 0
+        for i in _NUMBERS:
+            avg = mlib.add(avg, float(i))
             count += 1
-            num = ''
-        oread = file.read(1)
 
-avg = 0
+        avg = mlib.divide(avg, count)
+        s = 0
 
-for i in _NUMBERS:
-    avg = mlib.add(avg, float(i))
+        for i in _NUMBERS:
+            s = mlib.add(s ,mlib.exponentiate(float(i), 2))
 
-avg = mlib.divide(avg, count)
-s = 0
-
-for i in _NUMBERS:
-    s = mlib.add(s ,mlib.exponentiate(float(i), 2))
-
-s = mlib.subtract(s, mlib.multiply(count, mlib.exponentiate(avg, 2)))
-s = mlib.divide(s, mlib.subtract(count, 1))
-s = mlib.root(s, 2)
-print(s)
+        s = mlib.subtract(s, mlib.multiply(count, mlib.exponentiate(avg, 2)))
+        s = mlib.divide(s, mlib.subtract(count, 1))
+        s = mlib.root(s, 2)
+        print(s)
+        oread = file.readline()
