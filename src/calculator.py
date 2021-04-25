f1351b889b7192c4d8170cc941da84e2f1a91ea1
ivs <lanrosta@seznam.cz>
2021-04-25 21:16:55 +0200

    Reworked GUI integration with math library functions. Added functionality for all buttons and reworked fsm to filter some invalid combinations. Divided operators into unary and binary, reworked function calling.

--- src/calculator.py
+++ src/calculator.py
@@ -20,47 +20,132 @@ class Workspace:
 
     def __init__(self, printFn):
         self.state = self.State.TAKING_FIRST
-        self.operands = ['', '']
+        self.firstOperand = ''
+        self.firstOperand = ''
+        self.secondOperand = ''
         self.operator = ''
         self.printFn = printFn
 
+        self.unaryMap = {
+            'a': mlib.absolute,
+            'sin': mlib.sine,
+            'cos': mlib.cosine,
+            'tg': mlib.tangent,
+            '!': mlib.factorial,
+        }
+
+        self.binaryMap = {
+            '+': (mlib.add, False),
+            '-': (mlib.subtract, False),
+            '*': (mlib.multiply, False),
+            '/': (mlib.divide, False),
+            'r': (mlib.root, True),
+            '^': (mlib.exponentiate, False),
+            'log': (mlib.logarithm, True),
+        }
+
+    def clear(self):
+        self.operator = ''
+        self.firstOperand = ''
+        self.secondOperand = ''
+
     def readNew(self, symbol):
         # finite state machine
-        operators = list(("+", "-", "*", "/", "!", "r", "^", "a", "l", "s", "c", "t"))
+        binaryOperators = self.binaryMap.keys()
+        unaryOperators = self.unaryMap.keys()
 
         if self.state == self.State.TAKING_FIRST:
             if symbol == '=':
                 pass
 
-            elif symbol in operators:
+            elif symbol == '-' and self.firstOperand == '':
+                self.firstOperand = symbol
+
+            elif symbol == 'C':
+                self.clear()
+
+            elif symbol == 'CE':
+                if not self.operator == '':
+                    self.operator = ''
+
+                # deletes last numeral
+                elif not self.firstOperand == '':
+                    self.firstOperand = self.firstOperand[:-1]
+
+            elif symbol in unaryOperators:
+                if self.firstOperand == '':
+                    pass
+                else:
+                    self.operator = symbol
+                    self.compute()
+                    self.state = self.State.SHOWING_RESULT
+
+            elif symbol in binaryOperators:
                 self.operator = symbol
                 self.state = self.State.TAKING_SECOND
 
             else:
-                self.operands[0] += str(symbol)
+                self.firstOperand += str(symbol)
 
         elif self.state == self.State.TAKING_SECOND:
             if symbol == '=':
                 self.compute()
                 self.state = self.State.SHOWING_RESULT
 
-            elif symbol in operators:
-                self.operator = symbol
+            elif symbol == 'C':
+                self.clear()
+                self.state = self.State.TAKING_FIRST
+
+            elif symbol == 'CE':
+                # deletes last numeral
+                if not self.secondOperand == '':
+                    self.secondOperand = self.secondOperand[:-1]
+
+                elif not self.operator == '':
+                    self.operator = ''
+                    self.state = self.State.TAKING_FIRST
+
+                # deletes last numeral
+                elif not self.firstOperand == '':
+                    self.firstOperand = self.firstOperand[:-1]
+                    self.state = self.State.TAKING_FIRST
+
+            elif symbol in unaryOperators:
+                raise Exception('Invalid combination of operators')
+
+            elif symbol in binaryOperators:
+                pass
+            # maybe
 
             else:
-                self.operands[1] += str(symbol)
+                self.secondOperand += str(symbol)
 
         elif self.state == self.State.SHOWING_RESULT:
             if symbol == '=':
                 pass
 
-            elif symbol in operators:
+            elif symbol == 'C':
+                self.clear()
+                self.state = self.State.TAKING_FIRST
+
+            elif symbol == 'CE':
+                if not self.firstOperand == '':
+                    self.firstOperand = self.firstOperand[:-1]
+
+                else:
+                    self.state = self.State.TAKING_FIRST
+
+            elif symbol in unaryOperators:
+                self.operator = symbol
+                self.compute()
+
+            elif symbol in binaryOperators:
                 self.operator = symbol
                 self.state = self.State.TAKING_SECOND
 
             else:
                 self.state = self.State.TAKING_FIRST
-                self.operands[0] = str(symbol)
+                self.firstOperand = str(symbol)
 
         else:
             raise Exception('Cannot calculate')
@@ -68,79 +153,60 @@ class Workspace:
         self.show()
 
     def compute(self):
-        op0 = float(self.operands[0])
-        op1 = float(self.operands[1])
 
-        if self.operator == '+':
-            result = mlib.add(op0, op1)
+        op0 = float(self.firstOperand)
 
-        elif self.operator == '-':
-            result = mlib.subtract(op0, op1)
+        result = ''
+        if self.operator in self.unaryMap.keys():
+            fn = self.unaryMap[self.operator]
+            result = fn(op0)
 
-        elif self.operator == '*':
-            result = mlib.multiply(op0, op1)
+        elif self.operator in self.binaryMap.keys():
+            op1 = float(self.secondOperand)
+            fn = self.binaryMap[self.operator][0]
+            if self.binaryMap[self.operator][1]:
+                result = fn(op1, op0)
 
-        elif self.operator == '/':
-            result = mlib.divide(op0, op1)
-
-        elif self.operator == '!':
-            result = mlib.factorial(op0)
-
-        elif self.operator == 'r':
-            result = mlib.root(op1, op0)
-
-        elif self.operator == '^':
-            result = mlib.exponentiate(op0, op1)
-
-        elif self.operator == 'a':
-            result = mlib.absolute(op0)
-
-        elif self.operator == 'l':
-            result = mlib.logarithm(op0, op1)
-
-        elif self.operator == 's':
-            result = mlib.sine(op0)
-
-        elif self.operator == 'c':
-            result = mlib.cosine(op0)
-
-        elif self.operator == 't':
-            result = mlib.tangent(op0)
+            else:
+                result = fn(op0, op1)
 
         else:
             raise Exception('Unknown operator')
 
         result = round(result, 6)
 
-        # if result.is_integer():
-        #    result = int(result)
+        if float(result.is_integer()):
+            result = int(result)
 
-        self.operands = [str(result), '']
+        self.firstOperand = str(result)
+        self.secondOperand = ''
         self.operator = ''
 
     def show(self):
         printedOperator = self.operator
 
-        if printedOperator == 'r':
-            printedOperator = '√'
+        operatorMap = {
+            'r': '√',
+            '/': '÷',
+            '*': '×',
+        }
 
-        elif printedOperator == '/':
-            printedOperator = '÷'
+        if printedOperator in operatorMap.keys():
+            printedOperator = operatorMap[printedOperator]
 
-        elif printedOperator == 's':
-            printedOperator = 'sin'
+        if self.firstOperand == 'π':
+            self.firstOperand = round(mlib.PI, 6)
 
-        elif printedOperator == 'c':
-            printedOperator = 'cos'
+        if self.secondOperand == 'π':
+            self.secondOperand = round(mlib.PI, 6)
 
-        elif printedOperator == 't':
-            printedOperator = 'tg'
+        if self.firstOperand == 'e':
+            self.firstOperand = round(mlib.E, 6)
 
-        elif printedOperator == 'l':
-            printedOperator = 'log'
+        if self.secondOperand == 'e':
+            self.secondOperand = round(mlib.E, 6)
 
-
-        self.printFn(f'{self.operands[0]} {printedOperator} {self.operands[1]}')
+        self.printFn(f'{self.firstOperand} {printedOperator} {self.secondOperand}')
 
 
 class MainWindow(QtWidgets.QMainWindow):
@@ -152,7 +218,9 @@ class MainWindow(QtWidgets.QMainWindow):
         Then it sets keyboard shortcuts and shows the window.
         """
         super().__init__()
+
         #self.setWindowTitle('Calculator')
+
         uic.loadUi(path_to_ui, self)
 
         # print function is the argument of constructor
@@ -185,7 +253,12 @@ class MainWindow(QtWidgets.QMainWindow):
         noShortcuts = [
             self.pushButtonExponent,
             self.pushButtonRoot,
-            self.pushButtonFactorial,
+            self.pushButtonAbsolute,
+            self.pushButtonSinus,
+            self.pushButtonCosinus,
+            self.pushButtonTangens,
+            self.pushButtonClear,
+            self.pushButtonClearEntry,
         ]
 
         for removedButton in noShortcuts:
@@ -204,8 +277,10 @@ class MainWindow(QtWidgets.QMainWindow):
         specialButtons = [
             (self.pushButtonExponent, '^'),
             (self.pushButtonRoot, 'r'),
+            (self.pushButtonDivide, '/'),
+            (self.pushButtonMultiply, '*'),
             (self.pushButtonFactorial, '!'),
-            (self.pushButtonSinus, 's'),
+            (self.pushButtonAbsolute, 'a'),
         ]
 
         for button, symbol in specialButtons:
